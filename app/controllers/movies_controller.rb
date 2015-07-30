7396ace197e123ba16a30a964db132ce2a3331ee
ian schoonover <iantskon@gmail.com>
2015-07-30 17:39:24 +0200

    Update control flow for errors

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   def index
     # OMDB requires some query to be set or it will error
-    @query = params[:query] || "Godfather"
+    @query = params[:query]
 
     response = RestClient.get("http://www.omdbapi.com/?s=#{@query}")
     @movies = JSON.parse(response.body)['Search']
